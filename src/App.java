0158cfe84c7fd40b63d0cc199f9d99a9631e121d
Erick <eyungac3@est.ups.edu.ec>
2025-11-18 08:44:03 -0500

    update3

--- src/App.java
+++ src/App.java
@@ -26,17 +26,17 @@ public class App {
         new Person("Sofia", 24),
         };
         sC.mostrarArreglo(people);
-        bB.bubbleSort(people);
+        controlador.bubbleSort(people);
         sC.mostrarArreglo(people);
 
         Integer result1 = bB.busquedaBinaria(people,  28);
         sC.mostrarResultado(people, result1);
 
         sC.mostrarArreglo(arr);
-        bB.bubbleSort(arr);
+        controlador.bubbleSort(arr);
         sC.mostrarArreglo(arr);
 
-         Integer result2 = bB.busquedaBinaria(arr,  8);
+        Integer result2 = bB.busquedaBinaria(arr,  8);
         sC.mostrarResultado(arr, result2);
 
     }
--- src/controllers/PersonaController.java
+++ src/controllers/PersonaController.java
@@ -9,6 +9,43 @@ public class PersonaController {
     public PersonaController() {
     }
 
+    public void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    
+    public void bubbleSort(Person[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j].getEdad() > arr[j + 1].getEdad()) {
+                    Person temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            } 
+        }
+    }
     public PersonaController(Person persona) {
         this.persona = persona;
     }
